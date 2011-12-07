76ecdd26290372e51fb88f90105db94789bf3aef
OMJ- <mijames1@gmail.com>
2011-12-07 10:13:40 -0500

    fix market props

--- cm.mk
+++ cm.mk
@@ -235,7 +235,10 @@ $(call inherit-product, device/htc/vivow/media_htcaudio.mk)
 
 $(call inherit-product-if-exists, vendor/htc/vivow/vivow-vendor.mk)
 
+# Setup device specific product configuration.
 PRODUCT_NAME := cm_vivow
+PRODUCT_BRAND := verizon_wwe
 PRODUCT_DEVICE := vivow
-PRODUCT_MODEL := HTC Incredible 2
+PRODUCT_MODEL := Incredible 2
 PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.4/GRJ22/120826.3:user/release-keys PRIVATE_BUILD_DESC="4.08.605.3 CL120826 release-keys"
